b5c0aa05762b5b58d9f9695e10a797eed31e32d7
sant <santdev404@gmail.com>
2022-01-20 14:38:32 -0600

    create mock for gatewat class

--- tests/SubscriptionTest.php
+++ tests/SubscriptionTest.php
@@ -16,7 +16,7 @@ class SubscriptionTest extends TestCase
 
     /** @test */
     public function creating_a_subscription_marks_the_user_a_subscribed(){
-        $gateway = new FakeGateway;
+        $gateway = $this->createMock(Gateway::class);
         $subscription = new Subscription($gateway);
         $user = new User('JohnDoe');
 
